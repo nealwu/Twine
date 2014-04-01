a14ccccb4ab2765f98c882fbf28c260febf6e188
Neal Wu <neal@nealwu.com>
2014-03-31 23:16:51 -0400

    Add a new directive for binding and compiling HTML

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,26 +21,16 @@ app.config(function($routeProvider) {
     .otherwise({
       redirectTo: '/edit'
     });
-})
-.directive('compile', function($compile) {
-  // directive factory creates a link function
-  return function(scope, element, attrs) {
-    scope.$watch(
-      function(scope) {
-        // watch the 'compile' expression for changes
-        return scope.$eval(attrs.compile);
-      },
-      function(value) {
-        // when the 'compile' expression changes
-        // assign it into the current DOM
-        element.html(value);
+});
 
-        // compile the new DOM and link it to the current
-        // scope.
-        // NOTE: we only compile .childNodes so that
-        // we don't get into infinite loop compiling ourselves
+app.directive('ngHtmlCompile', function($compile) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.ngHtmlCompile, function(newValue) {
+        element.html(newValue);
         $compile(element.contents())(scope);
-      }
-    );
+      });
+    }
   };
 });
